728a2d62f1d06789883f7e846d7f61f3ee5b4975
Khadija <Khadija@Khadija-PC.esprit.tn>
2014-04-06 22:39:03 +0100

    Merge origin/master
    Conflicts:
    	connection/connection.php
    	dao/Evaluation_DAO.php
    	dao/Geolocalisation_DAO.php
    	dao/commentaire_dao.php
    	dao/domaine_dao.php
    	dao/lieu_dao.php
    	dao/reclamation_dao.php
    	dao/utilisateur_dao.php
    	entity/CommentaireEntity.php
    	entity/UtilisateurEntity.php
    	parsing/getXmlUtilisateur.php
    	parsing/reclamation_xmlparser.php
    	reclamations.php

---
+++ tunisianwatchme/src/com/tunisianwatchme/Handler/lieuHundler.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tunisianwatchme.Handler;
+import com.tunisianwatchme.Entity.Lieu;
+import org.kxml2.io.*;
+import org.xmlpull.v1.*;
+
+import java.io.*;
+import java.util.Vector;
+import javax.microedition.io.Connector;
+import javax.microedition.io.HttpConnection;
+
+/**
+ *
+ * @author Khadija
+ */
+public class lieuHundler extends Thread{
+    Vector lieuVector;
+
+    public lieuHundler(String URL) {
+        this.lieuVector = new Vector();
+    }
+  
+        public void run() {
+        try {
+            HttpConnection httpConnection = (HttpConnection) Connector.open("http://localhost/tw_mobile/domaines.php");
+            KXmlParser parser = new KXmlParser();
+            parser.setInput(new InputStreamReader(httpConnection.openInputStream()));
+            parser.nextTag();
+            parser.require(XmlPullParser.START_TAG, null, "lieux");
+            while (parser.nextTag() != XmlPullParser.END_TAG) {
+                readXMLData(parser);
+            }
+            parser.require(XmlPullParser.END_TAG, null, "lieux");
+            parser.next();
+            parser.require(XmlPullParser.END_DOCUMENT, null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readXMLData(KXmlParser parser)
+            throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "lieu");
+        Lieu lieu = new Lieu();
+        while (parser.nextTag() != XmlPullParser.END_TAG) {
+            parser.require(XmlPullParser.START_TAG, null, null);
+            String name = parser.getName();
+            String text = parser.nextText();
+            if (name.equals("id")) {
+                lieu.setId(Integer.parseInt(text));
+            } else if (name.equals("ville")) {
+                lieu.setNom(text);
+            }
+            parser.require(XmlPullParser.END_TAG, null, name);
+        }
+        lieuVector.addElement(lieu);
+        parser.require(XmlPullParser.END_TAG, null, "lieu");
+
+    }
+    
+}
