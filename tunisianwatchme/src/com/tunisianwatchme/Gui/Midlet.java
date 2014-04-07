7bee0645c80ae97963104390da2a04ff77f89a7e
marwensdiri <marwen.sdiri@esprit.tn>
2014-04-08 00:54:18 +0100

    Merge sdirimarwen/master
    Conflicts:
    	tunisianwatchme/src/com/tunisianwatchme/Gui/Midlet.java

--- tunisianwatchme/src/com/tunisianwatchme/Gui/Midlet.java
+++ tunisianwatchme/src/com/tunisianwatchme/Gui/Midlet.java
@@ -7,7 +7,9 @@ package com.tunisianwatchme.Gui;
  */
 import com.tunisianwatchme.Entity.Commentaire;
 import com.tunisianwatchme.Entity.Reclamation;
+import com.tunisianwatchme.Entity.Utilisateur;
 import com.tunisianwatchme.Handler.ReclamationHandler;
+import com.tunisianwatchme.Post.UtilisateurPost;
 import javax.microedition.midlet.*;
 
 /**
@@ -29,6 +31,41 @@ public class Midlet extends MIDlet {
             }
         }
         System.out.println(dh.getReclamation().size());
+        
+        
+        //        ReclamationHandler dh = new ReclamationHandler();
+//        System.out.println(dh.getReclamation());
+//        Reclamation rec = (Reclamation)dh.getReclamation().elementAt(0);
+//        System.out.println(rec.getId());
+        
+  /*
+        
+//        Image img = Image.createImage(null);
+        String s = "TmV3IHdvcmQgdG8gZW5jb2RlIHVzaW5nIEJhc2U2NCBhbHNvIHNwZWNpYWwgY2hhcnMgbGlrZSDRIGFuZCDz";
+        try {
+            String str = ImageUtils.decToString(s);
+            System.out.println(str);
+//            Image img = Image.createImage(str);
+//            f.append(img);
+//            disp.setCurrent(f);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+ */       
+        /**** test Utilisateur  ****/
+        
+        Utilisateur user = new Utilisateur();
+        user.setNom("Tiger");
+        user.setPrenom("Aymen");
+        user.setMail("mail");
+        user.setLogin("login");
+        user.setMdp("password");
+        user.setSexe('M');
+        
+        
+        UtilisateurPost userPost = new UtilisateurPost(user,'A');
+        userPost.start();
+        
     }
 
     public void pauseApp() {
