b0fe7581409e09d8be2149fb14f9066d64d68933
Skan <Skan@Garchi_Skander.esprit.tn>
2014-04-10 02:49:20 +0200

    Merge origin/master
    Conflicts:
    	tunisianwatchme/nbproject/project.properties
    	tunisianwatchme/src/com/tunisianwatchme/Gui/Midlet.java

--- tunisianwatchme/src/com/tunisianwatchme/Gui/Midlet.java
+++ tunisianwatchme/src/com/tunisianwatchme/Gui/Midlet.java
@@ -1,23 +1,28 @@
 package com.tunisianwatchme.Gui;
 
+import java.util.Vector;
 import com.tunisianwatchme.Entity.Domaine;
 import com.tunisianwatchme.Entity.Lieu;
 import com.tunisianwatchme.Entity.Reclamation;
+import com.tunisianwatchme.Entity.Stat;
 import com.tunisianwatchme.Entity.Utilisateur;
 import com.tunisianwatchme.Handler.DomaineHandler;
 import com.tunisianwatchme.Handler.LieuHandler;
 import com.tunisianwatchme.Handler.LoginHandler;
+import com.tunisianwatchme.Handler.StatHandler;
 import com.tunisianwatchme.Post.ReclamationPost;
+import de.enough.polish.ui.ChartItem;
 import de.enough.polish.ui.DateField;
+import de.enough.polish.ui.Style;
 import de.enough.polish.ui.TextField;
+import de.enough.polish.ui.UiAccess;
 import java.util.TimeZone;
-import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.ChoiceGroup;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
-import javax.microedition.lcdui.Command;
-import javax.microedition.lcdui.CommandListener;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
@@ -72,6 +77,17 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
 
     String type;
 
+    private Form StatForm;
+    private Command exitCommand = new Command("Exit", Command.EXIT, 3);
+    private Command styleLines = new Command("Lines", Command.SCREEN, 1);
+    private Command styleVerticalBar = new Command("Vertical Bars", Command.SCREEN, 1);
+    private Command stylePieChart = new Command("Pie Chart", Command.SCREEN, 1);
+    Vector v1;
+    Vector v2;
+    Vector v3;
+    DomaineHandler dh;
+    LieuHandler lh;
+
     public Midlet() {
         super();
         date.setDateFormatPattern("yyyy-MM-dd");
@@ -100,8 +116,7 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
         //#style textField
         this.menuScreen.append(Passfield);
 
-        //#style br
-        this.menuScreen.append(space);
+        
 
         //#style loginPng
         this.menuScreen.append(loginItem);
@@ -131,10 +146,11 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
         //#style flux1
         profil.append(ajoutReclamationItem);
 
-        //#style flux2
-        profil.append(GeolocalisationItem);
+        
         //#style flux3
         profil.append(StatItem);
+        //#style flux2
+        profil.append(GeolocalisationItem);
 
         profil.setCommandListener(this);
         profil.addCommand(this.logout);
@@ -176,6 +192,18 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
 
         ajout.setCommandListener(this);
         ajout.addCommand(cancel);
+
+        //Interface Statistiques
+        //#style test
+        this.StatForm = new Form("Statistiques");
+        this.StatForm.addCommand(this.exitCommand);
+        Command parent = new Command("selectionnez type", Command.SCREEN, 2);
+        this.StatForm.addCommand(parent);
+        UiAccess.addSubCommand(this.styleLines, parent, this.StatForm);
+        UiAccess.addSubCommand(this.styleVerticalBar, parent, this.StatForm);
+        UiAccess.addSubCommand(this.stylePieChart, parent, this.StatForm);
+        this.StatForm.setCommandListener(this);
+
     }
 
     protected void startApp() throws MIDletStateChangeException {
@@ -200,7 +228,16 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
             type = "ajout";
             Thread thr = new Thread(this);
             thr.start();
-
+        } else if (cmd == this.exitCommand) {
+            notifyDestroyed();
+        } else if (item == this.StatItem && cmd == this.select) {
+            System.out.println("success");
+            type = "chargement-stat";
+            Thread thr = new Thread(this);
+            thr.start();
+         updateChart();
+           
+            d.setCurrent(StatForm);
         }
         /*............................................................if (d.getCurrent() == this.menuScreen && cmd == this.cmdQuit) {
          notifyDestroyed();
@@ -222,9 +259,6 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
          }*/
     }
 
-    DomaineHandler dh;
-    LieuHandler lh;
-
     public void run() {
         if (d.getCurrent() == menuScreen) {
             LoginHandler loginHandler = new LoginHandler(Loginfield.getText(), Passfield.getText());
@@ -232,7 +266,7 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
             if (me != null) {
                 d.setCurrent(profil);
             }
-        } else if (d.getCurrent() == profil && type == "ajout") {
+        } else if (d.getCurrent() == profil && type.equals("ajout")) {
             dh = new DomaineHandler();
             for (int i = 0; i < dh.getDomaineVector().size(); i++) {
                 domaine.append(((Domaine) dh.getDomaineVector().elementAt(i)).toString(), null);
@@ -242,7 +276,7 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
                 lieu.append(((Lieu) lh.getLieuVector().elementAt(i)).toString(), null);
             }
             d.setCurrent(ajout);
-        } else if (d.getCurrent() == ajout && type == "submit") {
+        } else if (d.getCurrent() == ajout && type.equals("submit")) {
             Reclamation reclamation = new Reclamation();
             reclamation.setCitoyen(me);
             reclamation.setDate(date.getText());
@@ -268,8 +302,16 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
             System.out.println(reclamation.getDate());
             ReclamationPost rp = new ReclamationPost(reclamation, 1);
             rp.start();
-           // Alert alertSuccess = new Alert
+            // Alert alertSuccess = new Alert
 
+        } else if (d.getCurrent() == profil && type.equals("chargement-stat")) {
+            StatHandler sh1 = new StatHandler('l');
+            StatHandler sh2 = new StatHandler('e');
+            StatHandler sh3 = new StatHandler('d');
+            v1 = sh1.getstatVector();
+            v2 = sh2.getstatVector();
+            v3 = sh3.getstatVector();
+             
         }
 
     }
@@ -289,6 +331,33 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
             d.setCurrent(profil);
         } else if (disp == menuScreen && c == log) {
             Thread thr = new Thread(this);
+            thr.start();
+        } else if (c == this.styleLines) {
+            
+            Thread  thr= new Thread(new Runnable() {
+                public void run() {
+                    //#style lineChart
+                    updateChart();
+                }
+            });
+            thr.start();
+        } else if (c == this.styleVerticalBar) {
+           
+            Thread  thr= new Thread(new Runnable() {
+                public void run() {
+                    //#style verticalBarChart
+                    updateChart();
+                }
+            });
+            thr.start();
+        } else if (c == this.stylePieChart) {
+            
+           Thread  thr= new Thread(new Runnable() {
+                public void run() {
+                    //#style pieChart
+                    updateChart();
+                }
+            });
             thr.start();
         }
     }
@@ -337,5 +406,63 @@ public class Midlet extends MIDlet implements CommandListener, ItemCommandListen
 
      }
      */
+
+    private void updateChart() {
+        //#style lineChart
+        updateChart();
+    }
+
+    private void updateChart(Style style) {
+        this.StatForm.deleteAll();
+
+        int[] tab1 = new int[v1.size()];
+        for (int i = 0; i < v1.size(); i++) {
+            tab1[i] = ((Stat) v1.elementAt(i)).getValue();
+            System.out.println(tab1[i]);
+        }
+
+        int[] tab2 = new int[v2.size()];
+        for (int i = 0; i < v2.size(); i++) {
+            tab2[i] = ((Stat) v2.elementAt(i)).getValue();
+            System.out.println(tab2[i]);
+        }
+
+        int[] tab3 = new int[v3.size()];
+        for (int i = 0; i < v3.size(); i++) {
+            tab3[i] = ((Stat) v3.elementAt(i)).getValue();
+            System.out.println(tab3[i]);
+        }
+
+        int[] colors = new int[]{0xFF0000, 0x00FF00, 0x0000FF, 0x660033, 0x009999, 0xFFCC33, 0x00CC99, 0xFFFF33, 0x996600};
+        int[][] dataSequences = new int[][]{
+            tab1
+        };
+        ChartItem chart = new ChartItem("lieux:", dataSequences, colors, style);
+        chart.setLabelX("lieux");
+        chart.setLabelY("reclamations");
+        this.StatForm.append(chart);
+        /*
+         * TIGER conseil
+         * 
+         */
+        //this.form.append(new StringItem("hh", "xx"));
+
+        int[][] dataSequences2 = new int[][]{
+            tab2
+        };
+        ChartItem chart2 = new ChartItem("etablissements :", dataSequences2, colors, style);
+        chart2.setLabelX("etablissement");
+        chart2.setLabelY("reclamations");
+        this.StatForm.append(chart2);
+
+        int[][] dataSequences3 = new int[][]{
+            tab3
+        };
+        ChartItem chart3 = new ChartItem("domaines :", dataSequences3, colors, style);
+        chart3.setLabelX("domaines");
+        chart3.setLabelY("reclamations");
+        this.StatForm.append(chart3);
+       
+    }
 
 }
